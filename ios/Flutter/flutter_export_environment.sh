2459dca674f3d7a5c045f3d8758b934986a66e60
Vitor Nunes <vitor.almeida@mitraecp.com>
2025-01-27 18:34:21 -0300

    fix git ingnore

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/3.13.4/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/vitornunesdealmeida/Projetos/open-mitra-mobile/meu_projeto"
+export "FLUTTER_APPLICATION_PATH=/Users/vitornunesdealmeida/Projetos/open-mitra-mobile"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
